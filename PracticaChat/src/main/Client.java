e118690fced9043a1378000b1dde1859b4cff3a2
Hp EliteBook 840 G3 <Hp EliteBook 840 G3@DESKTOP-IR9JK3T>
2023-02-09 10:15:50 +0100

    hola

---
+++ PracticaChat/src/main/Client.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+  public static void main(String[] args) {
+    try {
+      Socket socket = new Socket("192.168.165.244", 1234);
+      System.out.println("Conectado al servidor");
+
+      DataInputStream in = new DataInputStream(socket.getInputStream());
+      DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+      Scanner scanner = new Scanner(System.in);
+      while (true) {
+        System.out.print("Escribe un mensaje: ");
+        String message = scanner.nextLine();
+
+        out.writeUTF(message);
+
+        message = in.readUTF();
+        System.out.println("Mensaje del servidor: " + message);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
